0dca4cffe79746a6e7796f0efe627235b5017a7f
ianlee <ianlee@hpc02.sbms.hku.hk>
2023-03-03 11:08:48 +0800

    update runXCMS and metID script

--- R/MultiableR.r
+++ R/MultiableR.r
@@ -1,10 +1,15 @@
-library(tidyverse)
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidyr)
+library(tibble)
 library(limma)
 library(ProteoMM)
 library(xcms)
 library(CAMERA)
 library(metid)
 
+#' @export
 runXCMS <- function (mzMLs, 
                      sample.info, 
                      polarity, 
@@ -22,7 +27,7 @@ runXCMS <- function (mzMLs,
                      camera.maxiso = 5
 ) {
   if (is.na(cwp)) {
-    cwp <- CentWaveParam(ppm = 10,
+    cwp <- xcms::CentWaveParam(ppm = 10,
                          snthr = 6,
                          peakwidth = c(10, 60), 
                          mzdiff = 0.01,
@@ -33,17 +38,21 @@ runXCMS <- function (mzMLs,
   }
   
   if (is.na(owp)) {
-    owp <- ObiwarpParam(binSize = 0.5)
+    owp <- xcms::ObiwarpParam(binSize = 0.5)
   } else if (!isClass(owp, "ObiwarpParam")) {
     stop("owp must be class ObiwarpParam!")
   }
   
-  if(is.null(sample.info$group)) {
+  if(is.null(sample.info$sample)) {
+    stop("sample.info must contain column `sample`!")
+  }else if(is.null(sample.info$group)) {
     stop("sample.info must contain column `group`!")
+  } else if (!all(sample.info$group %in% c("sample", "blank"))) {
+    stop('sample.info$group column contain labels other than "sample" or "blank"!')
   }
   
   if (is.na(pdp)) {
-    pdp <- PeakDensityParam(sampleGroups = sample.info$group,
+    pdp <- xcms::PeakDensityParam(sampleGroups = sample.info$group,
                             bw = 5,
                             binSize = 0.025,
                             minFraction = 0.5,
@@ -78,11 +87,15 @@ runXCMS <- function (mzMLs,
     }
   }
   
-  raw_data <- readMSData(files = mzMLs, pdata = new("NAnnotatedDataFrame", sample.info), mode = "onDisk")
+  # reading MSData
+  raw_data <- MSnbase::readMSData(files = mzMLs, pdata = new("NAnnotatedDataFrame", sample.info), mode = "onDisk")
   
-  xdata <- findChromPeaks(raw_data, param = cwp)
-  xdata2 <- adjustRtime(xdata, param = owp)
-  xdata3 <- groupChromPeaks(xdata2, param = pdp)
+  # detecting chromatographic peaks
+  xdata <- xcms::findChromPeaks(raw_data, param = cwp)
+  # aligning retention time
+  xdata2 <- xcms::adjustRtime(xdata, param = owp)
+  # grouping chrompeaks using sample vs blank
+  xdata3 <- xcms::groupChromPeaks(xdata2, param = pdp)
   
   if (save.xdata) {
     xdata.path <- file.path(save.location, "xdata.Rdata")
@@ -94,55 +107,95 @@ runXCMS <- function (mzMLs,
     save(xdata3, file = xdata3.path)
   }
   
-  primary_adducts <- paste0("rules/primary_adducts_", str_extract(polarity, "^.{3}"),".csv")
-  xsa <- xsAnnotate(as(filterMsLevel(xdata3, msLevel. = 1), "xcmsSet"), polarity = polarity)
-  anF <- groupFWHM(xsa, sigma = camera.sigma, perfwhm = camera.perfwhm, intval = camera.intval)
-  anI <- findIsotopes(anF, ppm = camera.ppm, mzabs = camera.mzabs, maxcharge = camera.maxcharge, maxiso = camera.maxiso, intval = camera.intval)
-  anIC <- groupCorr(anI)
-  anFA <- findAdducts(anIC, polarity=polarity, rules = read.csv(system.file(primary_adducts, package = "CAMERA")))
-  peaklist <- getPeaklist(anFA)
-  rownames(peaklist) <- rownames(featureDefinitions(xdata3))
+  polarity <- tolower(polarity)
+  primary.adducts.file <- paste0("rules/primary_adducts_", stringr::str_extract(polarity, "^.{3}"),".csv")
+  xsa <- CAMERA::xsAnnotate(as(MSnbase::filterMsLevel(xdata3, msLevel. = 1), "xcmsSet"), polarity = polarity)
+  anF <- CAMERA::groupFWHM(xsa, sigma = camera.sigma, perfwhm = camera.perfwhm, intval = camera.intval)
+  anI <- CAMERA::findIsotopes(anF, ppm = camera.ppm, mzabs = camera.mzabs, maxcharge = camera.maxcharge, maxiso = camera.maxiso, intval = camera.intval)
+  anIC <- CAMERA::groupCorr(anI)
+  anFA <- CAMERA::findAdducts(anIC, polarity=polarity, rules = read.csv(system.file(primary.adducts.file, package = "CAMERA")))
+  peaklist <- CAMERA::getPeaklist(anFA)
+  rownames(peaklist) <- rownames(xcms::featureDefinitions(xdata3))
   
-  xdata.table <- featureValues(xdata3) %>% as_tibble(rownames = "Feature.ID")
-  xdata.table <- xdata.table %>% pivot_longer(-Feature.ID) %>% mutate(name = str_remove(name, pattern = ".mzML")) %>% pivot_wider()
-  xdata.ftInfo <- featureDefinitions(xdata3) %>% as_tibble(rownames = "Feature.ID")
-  camera.table <- peaklist %>% as_tibble(rownames = "Feature.ID")
-  annotated.table <- xdata.ftInfo %>% select(Feature.ID, mzmed, rtmed) %>% inner_join(xdata.table, by = "Feature.ID") %>%
-    inner_join(camera.table %>% select(Feature.ID, isotopes, adduct, pcgroup), by = "Feature.ID") %>%
-    select(Feature.ID, mzmed, rtmed, sample.info %>% filter(group == "sample") %>% .$file,
-           sample.info %>% filter(group == "blank") %>% .$file, isotopes, adduct, pcgroup)
+  xdata.table <- xcms::featureValues(xdata3) %>% 
+    tidyr::as_tibble(rownames = "Feature.ID") %>% 
+    tidyr::pivot_longer(-Feature.ID) %>% 
+    dplyr::mutate(name = stringr::str_remove(name, pattern = ".mzML")) %>%
+    tidyr::pivot_wider()
+  xdata.ftInfo <- xcms::featureDefinitions(xdata3) %>% 
+    tidyr::as_tibble(rownames = "Feature.ID") %>% 
+    dplyr::select(Feature.ID, mzmed, rtmed)
+  camera.table <- peaklist %>% tidyr::as_tibble(rownames = "Feature.ID") %>% 
+    dplyr::select(Feature.ID, isotopes, adduct, pcgroup)
+  annotated.table <- xdata.ftInfo %>% dplyr::inner_join(xdata.table, by = "Feature.ID") %>%
+    dplyr::inner_join(camera.table, by = "Feature.ID") %>%
+    dplyr::select(Feature.ID, mzmed, rtmed, dplyr::filter(sample.info, group == "sample")$sample,
+           dplyr::filter(sample.info, group == "blank")$sample, isotopes, adduct, pcgroup)
   
   return(annotated.table)
-  
 }
 
-metid_annotate <- function (df, polarity, omic = "Mx") {
+#' @export
+metid_annotate <- function (df, polarity, omic = "Mx", database = NA, ms1.match.ppm = 15,
+                            rt.match.tol = 1000000) {
   load("database.RData")
-  database <- if (tolower(omic) == "lx") hmdb_lipids_metid else hmdb_metabo_metid
+  if (!is.na(database)) {
+    if (class(database) != "databaseClass") {
+      stop("Database must be class of metid::databaseClass!")
+    }
+  } else {
+    if (tolower(omic) == "lx") {
+      data("hmdb.lipids.massdb")
+      database <- hmdb.lipids.massdb
+    } else if (tolower(omic) == "mx") {
+      data("hmdb.mets.massdb")
+      database <- hmdb.mets.massdb
+    }
+    else {
+      stop("Unidentified omic: Lx or Mx; or provide manual database")
+    }
+  }
+  if (!("Feature.ID" %in% colnames(df))) {
+    stop("Cannot find column `Feature.ID` in dataframe!")
+  }
+  if ("mzmed" %in% colnames(df)) {
+    df <- df %>% dplyr::rename(mz = mzmed)
+  } else if (!("mz" %in% colnames(df))) {
+    stop("Cannot find column `mz` or `mzmed` in dataframe!")
+  }
+  if ("rtmed" %in% colnames(df)) {
+    df <- df %>% dplyr::rename(rt = rtmed)
+  } else if (!("rt" %in% colnames(df))) {
+    stop("Cannot find column `rt` or `rtmed` in dataframe!")
+  }
   tmp_file <- tempfile("metid", fileext = ".csv")
   for.metid.file <- basename(tmp_file)
-  df %>% select(Feature.ID, mz, rt) %>% write_csv(tmp_file)
+  on.exit(unlink(for.metid.file), add = TRUE)
+  df %>% dplyr::select(Feature.ID, mz, rt) %>% readr::write_csv(tmp_file)
   
+  polarity <- tolower(polarity)
   annotate_result <- metid::identify_metabolites(
     ms1.data = for.metid.file,
-    ms1.match.ppm = 15, 
-    rt.match.tol = 1000000,
+    ms1.match.ppm = ms1.match.ppm, 
+    rt.match.tol = rt.match.tol,
     path = tempdir(),
     polarity = polarity,
     database = database
   )
   
-  annotate_table <- annotate_result %>% get_identification_table(type = "new", candidate.num = 1) %>% rename(FeatureID = name)
+  annotate_table <- annotate_result %>% metid::get_identification_table(type = "new", candidate.num = 1) %>%
+    dplyr::rename(Feature.ID = name)
   
   return (annotate_table)
 }
 
+#' @export
 write_lipidfinder_csv <- function(df, file.name) {
   lf_output <- df %>% replace(is.na(.), 0) %>%
-    mutate(Feature.ID = as.numeric(str_replace(Feature.ID, "FT", ""))) %>%
-    rename(FeatureID = Feature.ID)
+    dplyr::mutate(Feature.ID = as.numeric(stringr::str_extract(Feature.ID, "([0-9]+)", group = 1))) %>%
+    dplyr::rename(FeatureID = Feature.ID)
   
-  write_csv(lf_output, file = file.name)
+  readr::write_csv(lf_output, file = file.name)
 }
 
 #' Read csv files from LipidFinder output
@@ -156,37 +209,38 @@ write_lipidfinder_csv <- function(df, file.name) {
 #' @return a tibble dataframe
 #' @export
 read_LipidFinder_csv <- function(file.path, omic = "Mx") {
-    df <- read_csv(file.path)
+    df <- readr::read_csv(file.path)
     if (omic == "Lx") {
-        df <- df %>% mutate(Omic = "Lx")
+        df <- df %>% dplyr::mutate(Omic = "Lx")
     } else if (omic == "Mx") {
-        df <- df %>% mutate(Omic = "Mx")
+        df <- df %>% dplyr::mutate(Omic = "Mx")
     } else {
         stop("Unknown omic profile. Omic options:[\"Lx\", \"Mx\"]")
     }
     fid_length <- nchar(max(df$FeatureID))
     df <- df %>%
-            mutate(FeatureID = paste0(str_extract(Omic, "^(.)"),
-                                      str_extract(Polarity, "^(.)"),
-                                      str_pad(FeatureID, fid_length, side = "left", pad = "0"))) %>%
-            select(Omic, Polarity, everything()) %>%
-            pivot_longer(ends_with("mean"), names_to = "sample") %>%
-            mutate(sample = str_replace(sample, "_mean", "")) %>%
-            pivot_wider(names_from = "sample")
+      dplyr::mutate(FeatureID = paste0(stringr::str_extract(Omic, "^(.)"),
+                                       stringr::str_extract(Polarity, "^(.)"),
+                                       stringr::str_pad(FeatureID, fid_length, side = "left", pad = "0"))) %>%
+      dplyr::select(Omic, Polarity, tidyselect::everything()) %>%
+      dplyr::rename(Feature.ID = FeatureID) %>%
+      tidyr::pivot_longer(ends_with("mean"), names_to = "sample") %>%
+      dplyr::mutate(sample = stringr::str_replace(sample, "_mean", "")) %>%
+      tidyr::pivot_wider(names_from = "sample")
     return(df)
 }
 
 #' Data frame preprocesssing
 #'
 #' The function preprocess the data by filtering, imputing, and transforming
-#' @param df the tibble containing only FeatureIDs and sample abundance
+#' @param df the tibble containing only Feature.IDs and sample abundance
 #' @param filter_threshold minimum number of non NAs to keep the feature
 #' @param impute imputation method. "gm2" = half of global minimum, "gm5" = a fifth of global minimum
 #' @param log_base log transform base. Default = 2
 #' @return processed and transformed tibble data frame
 #' @export
 tbManipulate <- function(df, filter_threshold, impute = "gm2", log_base = 2) {
-    df.m <- df %>% column_to_rownames("FeatureID") %>% as.matrix()
+    df.m <- df %>% tibble::column_to_rownames("Feature.ID") %>% as.matrix()
     thres <- filter_threshold
     filtered.df <- df.m[rowSums(!is.na(df.m)) > thres,]
 
@@ -204,31 +258,31 @@ tbManipulate <- function(df, filter_threshold, impute = "gm2", log_base = 2) {
 
     log.df <- log(imputed.df, base = log_base)
 
-    return(as_tibble(log.df, rownames = "FeatureID"))
+    return(tibble::as_tibble(log.df, rownames = "Feature.ID"))
 }
 
 #' EigenMS normalisation
 #'
 #' Use the ProteoMM package to normalise the dataframe using EigenMS
-#' @param df the tibble containing only FeatureIDs and sample abundance
+#' @param df the tibble containing only Feature.IDs and sample abundance
 #' @param sample.info sample info. Must contain a column named "sample"
 #' for the sample information and a column named "group" for the experimental group
 #' @return a EigenMS normalised tibble dataframe
 #' @export
 normEigenMS <- function(df, sample.info) {
-    if (!all(sort(sample.info$sample) == sort(colnames(df %>% select(-FeatureID))))) {
+    if (!all(sort(sample.info$sample) == sort(colnames(df %>% dplyr::select(-Feature.ID))))) {
          stop('sample name not match between sample.info and df')
     }
 
-    m_prot.info <- df %>% select(FeatureID) %>% as.data.frame()
+    m_prot.info <- df %>% dplyr::select(Feature.ID) %>% as.data.frame()
 
     sample.treatment <- as.factor(sample.info$group)
-    m_logInts <- df %>% select(sample.info$sample) %>% as.data.frame()
+    m_logInts <- df %>% dplyr::select(sample.info$sample) %>% as.data.frame()
 
     hs_m_ints_eig1 <- ProteoMM::eig_norm1(m=m_logInts,treatment=sample.treatment,prot.info=m_prot.info)
     if(hs_m_ints_eig1$h.c > 0) {
         hs_m_ints_norm <- ProteoMM::eig_norm2(rv=hs_m_ints_eig1)
-        output <- as_tibble(hs_m_ints_norm$norm_m, rownames = "FeatureID")
+        output <- tibble::as_tibble(hs_m_ints_norm$norm_m, rownames = "Feature.ID")
     } else {
         message("WARNING: No bias trends detected!")
         output <- df
@@ -239,33 +293,33 @@ normEigenMS <- function(df, sample.info) {
 #' Median normalisation
 #'
 #' Median normalise the dataframe
-#' @param df the tibble containing only FeatureIDs and sample abundance
+#' @param df the tibble containing only Feature.IDs and sample abundance
 #' @param mean the average value to normalise the dataframe to. Default as NA
 #' which normalise the mean of the dataframe to the mean of the medians
 #' @return a Median normalised tibble dataframe
 #' @export
 normMedian <- function(df, mean = NA) {
-    rowMeans <- df %>% summarise_if(is.numeric, median, na.rm = TRUE) %>% pivot_longer(cols = everything(), values_to = "median")
+    rowMeans <- df %>% dplyr::summarise_if(is.numeric, median, na.rm = TRUE) %>% tidyr::pivot_longer(cols = tidyselect::everything(), values_to = "median")
     if (is.na(mean)) {
-        overallMean <- rowMeans %>% summarise(mean = mean(median)) %>% .$mean
+        overallMean <- rowMeans %>% dplyr::summarise(mean = mean(median)) %>% .$mean
     } else {
         overallMean <- mean
     }
-    median.df <- df %>% pivot_longer(-FeatureID) %>% inner_join(rowMeans, by = "name") %>%
-                        mutate(value = value - median + overallMean) %>% select(-median) %>% pivot_wider()
+    median.df <- df %>% tidyr::pivot_longer(-Feature.ID) %>% dplyr::inner_join(rowMeans, by = "name") %>%
+                        dplyr::mutate(value = value - median + overallMean) %>% dplyr::select(-median) %>% tidyr::pivot_wider()
     return(median.df)
 }
 
 #' Limma differential analysis
 #'
 #' Perform limma differential analysis
-#' @param df the tibble containing only FeatureIDs and sample abundance
+#' @param df the tibble containing only Feature.IDs and sample abundance
 #' @param design.matrix limma design matrix. See limma
 #' @param cont.matrix limma cont matrix. See limma
 #' @return The log-fold change and p value of each feature per comparison
 #' @export
 runLimma <- function(df, design.matrix, cont.matrix) {
-    limma.fit <- df %>% column_to_rownames(var = "FeatureID") %>% as.matrix() %>%
+    limma.fit <- df %>% tibble::column_to_rownames(var = "Feature.ID") %>% as.matrix() %>%
       limma::lmFit(design.matrix) %>%
       limma::contrasts.fit(cont.matrix) %>%
       limma::eBayes()
@@ -273,11 +327,36 @@ runLimma <- function(df, design.matrix, cont.matrix) {
     comparisons <- colnames(cont.matrix)
     limma.tb.list <- list()
     for(i in 1:length(comparisons)) {
-        limma.tb <- limma.fit %>% limma::topTable(coef = comparisons[i], adjust.method = "BH", number = Inf) %>% as_tibble(rownames = "FeatureID") %>% mutate(coef = comparisons[i])
+        limma.tb <- limma.fit %>% limma::topTable(coef = comparisons[i], adjust.method = "BH", number = Inf) %>% tidyr::as_tibble(rownames = "Feature.ID") %>% dplyr::mutate(coef = comparisons[i])
         limma.tb.list[[i]] <- limma.tb
     }
-    limma.summary <- bind_rows(limma.tb.list) %>% select(FeatureID, coef, logFC, adj.P.Val) %>%
-            pivot_longer(c(-FeatureID, -coef)) %>% mutate(colname = paste(name, coef, sep = ".")) %>%
-            select(FeatureID, colname, value) %>% pivot_wider(names_from = "colname")
+    limma.summary <- dplyr::bind_rows(limma.tb.list) %>% dplyr::select(Feature.ID, coef, logFC, adj.P.Val) %>%
+            tidyr::pivot_longer(c(-Feature.ID, -coef)) %>% dplyr::mutate(colname = paste(name, coef, sep = ".")) %>%
+            dplyr::select(Feature.ID, colname, value) %>% tidyr::pivot_wider(names_from = "colname")
     return(limma.summary)
+}
+
+#' @export
+group_annotation <- function(df, annotated.df, match.id = "HMDB.ID") {
+  if (!(match.id %in% colnames(annotated.df))) {
+    stop(paste0("Column `", match.id, "` not found in annotation dataframe!"))
+  }
+  
+  grouped.df = annotated.df %>% 
+    dplyr::rename(Match.ID = tidyselect::matches(paste0("^",match.id,"$"))) %>% 
+    dplyr::select(Omic, Feature.ID, Match.ID) %>% 
+    dplyr::inner_join(df %>% dplyr::ungroup() %>% dplyr::select(-Omic, -Polarity), by = "Feature.ID") %>%
+    dplyr::filter(!is.na(Match.ID)) %>% 
+    dplyr::rowwise() %>% 
+    dplyr::mutate(rowmed = median(dplyr::c_across(tidyselect::where(is.numeric))),
+                  rowmean = mean(dplyr::c_across(tidyselect::where(is.numeric))),
+                  rowmax = max(dplyr::c_across(tidyselect::where(is.numeric)))) %>% 
+    dplyr::group_by(Match.ID) %>% 
+    dplyr::slice_max(rowmed) %>% 
+    dplyr::slice_max(rowmean) %>%
+    dplyr::slice_max(rowmax, n = 1) %>%
+    dplyr::select(-c(rowmean, rowmed, rowmax, Feature.ID)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::rename(!!match.id := Match.ID)
+  return(grouped.df)
 }
